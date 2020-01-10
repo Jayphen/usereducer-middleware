e284a556f097857e7cdab2b25c123cd7bb527b53
Jayphen <jayphen@hyperlab.se>
2020-01-10 11:29:28 +0100

    Example of middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+
+const initialItems: Item[] = [
+  { id: 1, text: "blah" },
+  { id: 2, text: "gdfg" },
+  { id: 3, text: "sdfgs" },
+  { id: 4, text: "hello there" }
+];
 
 const IndexPage: NextPage = () => {
+  const [state, dispatch] = React.useReducer(reducer, initialItems);
+  const send = dispatchMiddleware(dispatch);
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
@@ -12,8 +22,71 @@ const IndexPage: NextPage = () => {
           <a>About</a>
         </Link>
       </p>
+
+      <div>
+        {state.map(item => {
+          return (
+            <div key={item.id}>
+              {item.text}
+              <button
+                onClick={() =>
+                  send({ type: "REMOVE", payload: { id: item.id } })
+                }
+              >
+                Delete item
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </Layout>
-  )
+  );
+};
+
+function reducer(state: Item[], action: Action): Item[] {
+  switch (action.type) {
+    case "ADD":
+      return [...state, { id: action.payload.id, text: action.payload.text }];
+
+    case "REMOVE": {
+      const newState = [...state];
+      return newState.filter(item => item.id !== action.payload.id);
+    }
+
+    default:
+      return state;
+  }
 }
 
-export default IndexPage
+type Action =
+  | {
+      type: "ADD";
+      payload: Item;
+    }
+  | { type: "REMOVE"; payload: { id: number } };
+
+interface Item {
+  id: number;
+  text: string;
+}
+
+function dispatchMiddleware(dispatch: React.Dispatch<Action>) {
+  return (action: Action) => {
+    switch (action.type) {
+      case "ADD":
+        console.log("adding", action.payload);
+        dispatch(action);
+        break;
+
+      case "REMOVE":
+        console.log("removing", action.payload);
+        dispatch(action);
+        break;
+
+      default:
+        break;
+    }
+  };
+}
+
+export default IndexPage;
